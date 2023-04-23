45b2601cfe33a2c47e76d1345eafd45b818e21f4
ninaL90 <122303369+ninaL90@users.noreply.github.com>
2023-04-22 22:00:38 -0400

    Update write.py
    This is a new feature I added. This allows you to assign the RFID a name before writing on it.

--- examples/write.py
+++ examples/write.py
@@ -1,6 +1,44 @@
 import mfrc522
 from os import uname
 
+#I added the following code so that you can assign the tag a specific name or code before writing on it
+
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+import time
+
+ledb= 3
+ledr= 5
+buz= 7
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(ledb,GPIO.OUT)
+GPIO.setup(ledr,GPIO.OUT)
+GPIO.setup(buz,GPIO.OUT)
+reader = SimpleMFRC522()
+
+try:
+        GPIO.output(ledb,1)
+        GPIO.output(ledr,0)
+        GPIO.output(buz,0)
+        reg_no = raw_input('Registration Number:')
+        GPIO.output(ledb,1)
+        GPIO.output(ledr,1)
+        GPIO.output(buz,0)
+        print("Now place your tag to write")
+        reader.write(reg_no)
+        print("Written")
+        GPIO.output(ledb,0)
+        GPIO.output(ledr,0)
+        GPIO.output(buz,1)
+        time.sleep(0.5)
+        GPIO.output(ledb,0)
+        GPIO.output(ledr,0)
+        GPIO.output(buz,0)
+
+finally:
+        GPIO.cleanup()
 
 def do_write():
 
